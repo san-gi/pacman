57c924fb7a7567fa12ae80cf678c8acf1c9aac86
Jujulodace <damien.deturck@etu.u-bordeaux.fr>
2020-05-11 12:39:23 -0400

    sav

--- save.php
+++ save.php
@@ -1,6 +1,9 @@
 <?php
-if (isset($_POST['jeu'])) {
-    file_put_contents(date('d/m/o H:i:s'),$_POST['jeu']);
-}
+
+    $fill = fopen("/home/sangi/Games/".date('d/m/o H:i:s').".json","w");
+    fwrite($fill, "oui");
+    fclose($fill);
+    //file_put_contents(date('d/m/o H:i:s'),"oui");
+
 
 ?>
