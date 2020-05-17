2c198217756bc51040bd4d7432dead33872cd387
sangi <jujulodace@gmail.com>
2020-05-17 14:26:51 -0400

    mini correctif

--- static/script.js
+++ static/script.js
@@ -20,7 +20,7 @@ var lvl = 1;
 var mode = "scatter";
 var FantomeBleu = false;
 var fantomeEat = 200;
-var socket = io.connect('192.168.2.97');
+var socket = io.connect('sangigi.ddns.net');
 var logs = {}
 var d = new Date().getTime();
 var SortieInky;
